f8e91838a055b0e11fdafcd1ea87bc294511e432
Kyle Shifflett <kyle@shardus.com>
2021-03-25 20:30:48 -0700

    Back to original

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,55 +1,55 @@
-import App from "next/app";
-import React from "react";
-import { io } from "socket.io-client";
+// import App from "next/app";
+// import React from "react";
+// import { io } from "socket.io-client";
 
-class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+// class MyApp extends App {
+//   static async getInitialProps({ Component, ctx }) {
+//     let pageProps = {};
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
+//     if (Component.getInitialProps) {
+//       pageProps = await Component.getInitialProps(ctx);
+//     }
 
-    return { pageProps };
-  }
-  state = {
-    socket: null,
-  };
-  componentDidMount() {
-    // connect to WS server and listen event
-    const socket = io();
-    this.setState({ socket });
-  }
+//     return { pageProps };
+//   }
+//   state = {
+//     socket: null,
+//   };
+//   componentDidMount() {
+//     // connect to WS server and listen event
+//     const socket = io();
+//     this.setState({ socket });
+//   }
 
-  // close socket connection
-  componentWillUnmount() {
-    this.state.socket.close();
-  }
+//   // close socket connection
+//   componentWillUnmount() {
+//     this.state.socket.close();
+//   }
 
-  render() {
-    const { Component, pageProps } = this.props;
-    return <Component {...pageProps} socket={this.state.socket} />;
-  }
-}
-
-export default MyApp;
-
-// import "@styles/globals.css";
-// import Navbar from "@components/Navbar";
-// import { UserContext } from "@lib/context";
-// import { useUserData } from "@lib/hooks";
-// import { Toaster } from "react-hot-toast";
-
-// function MyApp({ Component, pageProps }) {
-//   const userData = useUserData();
-
-//   return (
-//     <UserContext.Provider value={userData}>
-//       <Navbar />
-//       <Component {...pageProps} />
-//       <Toaster />
-//     </UserContext.Provider>
-//   );
+//   render() {
+//     const { Component, pageProps } = this.props;
+//     return <Component {...pageProps} socket={this.state.socket} />;
+//   }
 // }
 
 // export default MyApp;
+
+import "@styles/globals.css";
+import Navbar from "@components/Navbar";
+import { UserContext } from "@lib/context";
+import { useUserData } from "@lib/hooks/useUserData";
+import { Toaster } from "react-hot-toast";
+
+function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
+
+  return (
+    <UserContext.Provider value={userData}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </UserContext.Provider>
+  );
+}
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,164 +1,164 @@
-import { useState } from "react";
-import Link from "next/link";
-import fetch from "isomorphic-unfetch";
-import useSocket from "@lib/hooks/useSocket";
-
-export default function ChatOne(props) {
-  const [field, setField] = useState("");
-  const [newMessage, setNewMessage] = useState(0);
-  const [messages, setMessages] = useState(props.messages || []);
-
-  const socket = useSocket("message.chat1", (message) => {
-    setMessages((messages) => [...messages, message]);
-  });
-
-  useSocket("message.chat2", () => {
-    setNewMessage((newMessage) => newMessage + 1);
-  });
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // create message object
-    const message = {
-      id: new Date().getTime(),
-      value: field,
-    };
-
-    // send object to WS server
-    socket.emit("message.chat1", message);
-    setField("");
-    setMessages((messages) => [...messages, message]);
-  };
-
-  return (
-    <main>
-      <div>
-        <Link href="/">
-          <a>{"Chat One"}</a>
-        </Link>
-        <br />
-        <Link href="/clone">
-          <a>
-            {`Chat Two${
-              newMessage > 0 ? ` ( ${newMessage} new message )` : ""
-            }`}
-          </a>
-        </Link>
-        <ul>
-          {messages.map((message) => (
-            <li key={message.id}>{message.value}</li>
-          ))}
-        </ul>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <input
-            onChange={(e) => setField(e.target.value)}
-            type="text"
-            placeholder="Hello world!"
-            value={field}
-          />
-          <button>Send</button>
-        </form>
-      </div>
-    </main>
-  );
-}
-
-ChatOne.getInitialProps = async () => {
-  const response = await fetch("http://localhost:3000/messages/chat1");
-  const messages = await response.json();
-
-  return { messages };
-};
-
-// import PostFeed from "@components/PostFeed";
-// import Metatags from "@components/Metatags";
-// import Loader from "@components/Loader";
-// import { firestore, fromMillis, postToJSON } from "@lib/firebase";
-
-// import useSocket from "@lib/hooks/useSocket";
 // import { useState } from "react";
+// import Link from "next/link";
+// import fetch from "isomorphic-unfetch";
+// import useSocket from "@lib/hooks/useSocket";
 
-// // Max post to query per page
-// const LIMIT = 10;
+// export default function ChatOne(props) {
+//   const [field, setField] = useState("");
+//   const [newMessage, setNewMessage] = useState(0);
+//   const [messages, setMessages] = useState(props.messages || []);
 
-// export async function getServerSideProps(context) {
-//   const postsQuery = firestore
-//     .collectionGroup("posts")
-//     .where("published", "==", true)
-//     .orderBy("createdAt", "desc")
-//     .limit(LIMIT);
+//   const socket = useSocket("message.chat1", (message) => {
+//     setMessages((messages) => [...messages, message]);
+//   });
 
-//   const posts = (await postsQuery.get()).docs.map(postToJSON);
+//   useSocket("message.chat2", () => {
+//     setNewMessage((newMessage) => newMessage + 1);
+//   });
 
-//   return {
-//     props: { posts }, // will be passed to the page component as props
-//   };
-// }
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
 
-// export default function Home(props) {
-//   const [posts, setPosts] = useState(props.posts);
-//   const [loading, setLoading] = useState(false);
+//     // create message object
+//     const message = {
+//       id: new Date().getTime(),
+//       value: field,
+//     };
 
-//   const [postsEnd, setPostsEnd] = useState(false);
-
-//   // Get next page in pagination query
-//   const getMorePosts = async () => {
-//     setLoading(true);
-//     const last = posts[posts.length - 1];
-
-//     const cursor =
-//       typeof last.createdAt === "number"
-//         ? fromMillis(last.createdAt)
-//         : last.createdAt;
-
-//     const query = firestore
-//       .collectionGroup("posts")
-//       .where("published", "==", true)
-//       .orderBy("createdAt", "desc")
-//       .startAfter(cursor)
-//       .limit(LIMIT);
-
-//     const newPosts = (await query.get()).docs.map((doc) => doc.data());
-
-//     setPosts(posts.concat(newPosts));
-//     setLoading(false);
-
-//     if (newPosts.length < LIMIT) {
-//       setPostsEnd(true);
-//     }
+//     // send object to WS server
+//     socket.emit("message.chat1", message);
+//     setField("");
+//     setMessages((messages) => [...messages, message]);
 //   };
 
 //   return (
 //     <main>
-//       <Metatags
-//         title="Home Page"
-//         description="Get the latest posts on our site"
-//       />
-
-//       <div className="card card-info">
-//         <h2>💡 Next.js + Firebase - The Full Course</h2>
-//         <h3> TEST </h3>
-//         <p>
-//           Welcome! This app is built with Next.js and Firebase and is loosely
-//           inspired by Dev.to.
-//         </p>
-//         <p>
-//           Sign up for an 👨‍🎤 account, ✍️ write posts, then 💞 heart content
-//           created by other users. All public content is server-rendered and
-//           search-engine optimized.
-//         </p>
+//       <div>
+//         <Link href="/">
+//           <a>{"Chat One"}</a>
+//         </Link>
+//         <br />
+//         <Link href="/clone">
+//           <a>
+//             {`Chat Two${
+//               newMessage > 0 ? ` ( ${newMessage} new message )` : ""
+//             }`}
+//           </a>
+//         </Link>
+//         <ul>
+//           {messages.map((message) => (
+//             <li key={message.id}>{message.value}</li>
+//           ))}
+//         </ul>
+//         <form onSubmit={(e) => handleSubmit(e)}>
+//           <input
+//             onChange={(e) => setField(e.target.value)}
+//             type="text"
+//             placeholder="Hello world!"
+//             value={field}
+//           />
+//           <button>Send</button>
+//         </form>
 //       </div>
-
-//       <PostFeed posts={posts} admin={null} />
-
-//       {!loading && !postsEnd && (
-//         <button onClick={getMorePosts}>Load more</button>
-//       )}
-
-//       <Loader show={loading} />
-
-//       {postsEnd && "You have reached the end!"}
 //     </main>
 //   );
 // }
+
+// ChatOne.getInitialProps = async () => {
+//   const response = await fetch("http://localhost:3000/messages/chat1");
+//   const messages = await response.json();
+
+//   return { messages };
+// };
+
+import PostFeed from "@components/PostFeed";
+import Metatags from "@components/Metatags";
+import Loader from "@components/Loader";
+import { firestore, fromMillis, postToJSON } from "@lib/firebase";
+
+import useSocket from "@lib/hooks/useSocket";
+import { useState } from "react";
+
+// Max post to query per page
+const LIMIT = 10;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup("posts")
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  // Get next page in pagination query
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor =
+      typeof last.createdAt === "number"
+        ? fromMillis(last.createdAt)
+        : last.createdAt;
+
+    const query = firestore
+      .collectionGroup("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
+  return (
+    <main>
+      <Metatags
+        title="Home Page"
+        description="Get the latest posts on our site"
+      />
+
+      <div className="card card-info">
+        <h2>💡 Next.js + Firebase - The Full Course</h2>
+        <h3> TEST </h3>
+        <p>
+          Welcome! This app is built with Next.js and Firebase and is loosely
+          inspired by Dev.to.
+        </p>
+        <p>
+          Sign up for an 👨‍🎤 account, ✍️ write posts, then 💞 heart content
+          created by other users. All public content is server-rendered and
+          search-engine optimized.
+        </p>
+      </div>
+
+      <PostFeed posts={posts} admin={null} />
+
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load more</button>
+      )}
+
+      <Loader show={loading} />
+
+      {postsEnd && "You have reached the end!"}
+    </main>
+  );
+}
